ef88026f71b5f984e2bb9171d591c8eac075226a
Smustus <rikardfors_91@hotmail.com>
2024-09-30 18:11:23 +0200

    added moviedisplay functionality, fetching and routing
    user can se trending and recommended movies (choosen randomly)

---
+++ src/components/movieDisplay/MovieDisplay.tsx
@@ -0,0 +1,24 @@
+import './MovieDisplay.css';
+
+type MovieDisplayProps = {
+ movies: Movie[]
+ title: string
+}
+
+export const MovieDisplay = ({movies, title}: MovieDisplayProps) => {
+
+  return (
+    <section className="displayMovie-container">
+      <h3>{title}</h3>
+      <section className="displayMovie-container-movies">
+      {
+        movies.map((movie) => (
+          <article key={movie.title}>
+            <h4>{movie.title}</h4>
+          </article>
+        ))
+      }
+      </section>
+    </section>
+  )
+}
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react'
+import './Home.css';
+import { MovieDisplay } from '../../components/movieDisplay/MovieDisplay';
+import { fetchMovieData } from '../../utilities/fetch';
+
+export const Home = () => {
+
+  const [recommended, setRecommended] = useState<Movie[]>([]);
+  const [trending, setTrending] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    async function fetchData(){
+      try {
+        const data: Movie[] = await fetchMovieData();
+      
+        if(data.length > 0){
+          const trendingMovies = randomSelectionMovies(data, 8);
+          console.log(trendingMovies);
+          
+          setTrending(trendingMovies);
+
+          const filteredMovies = remainingMovies(trendingMovies, data);
+          const recommendedMovies = randomSelectionMovies(filteredMovies, 8);
+          console.log(recommendedMovies);
+          
+          setRecommended(recommendedMovies);
+        };
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const remainingMovies = (trendingMovies: Movie[], allMovies: Movie[]) => {
+    return allMovies.filter((movieData) => {
+      return !trendingMovies.some((movie) => movie.title === movieData.title)
+    });
+  };
+
+  const randomSelectionMovies = (movies: Movie[], count: number) => {
+    const randomIndices: number[] = [];
+
+    while (randomIndices.length < count) {
+      const randomNum = Math.floor(Math.random() * movies.length);
+      if (!randomIndices.includes(randomNum)) {
+        randomIndices.push(randomNum);
+      }
+    }
+    const selectedMovies = randomIndices.map((index) => movies[index]);
+    return selectedMovies;
+  };
+  return (
+    <main>
+      <section>
+        <MovieDisplay movies={trending} title='Trending' />
+      </section>
+      <section>
+        <MovieDisplay movies={recommended} title='Recommended'/>
+      </section>
+    </main>
+  )
+}
