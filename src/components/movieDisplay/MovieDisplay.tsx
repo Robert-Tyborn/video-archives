2031e2dceb2e9e5e0d7f3bdeed5dd4bedcb28e50
Oscar <oscar.la02@outlook.com>
2024-10-01 13:25:07 +0200

    added carousel function to the moviedisplay
    Created a new component, created the carousel function and combined it with the moviedisplay component. Styling may be changed later

--- src/components/movieDisplay/MovieDisplay.tsx
+++ src/components/movieDisplay/MovieDisplay.tsx
@@ -1,24 +1,34 @@
+import { Carousel } from '../carousel/Carousel';
 import './MovieDisplay.css';
 
+type Movie = {
+  title: string;
+  thumbnail: string;
+};
+
 type MovieDisplayProps = {
- movies: Movie[]
- title: string
-}
+  movies: Movie[];
+  title: string;
+  thumbnail: string;
+};
 
-export const MovieDisplay = ({movies, title}: MovieDisplayProps) => {
-
+export const MovieDisplay = ({ movies, title }: MovieDisplayProps) => {
   return (
-    <section className="displayMovie-container" >
+    <section className="displayMovie-container">
       <h3>{title}</h3>
-      <section className="displayMovie-container-movies" >
-      {
-        movies.map((movie) => (
-          <article key={movie.title} data-testid='movieCard'>
-            <h4>{movie.title}</h4>
-          </article>
-        ))
-      }
-      </section>
+      <Carousel>
+        <section className="displayMovie-container-movies">
+          {movies.map(movie => (
+            <article
+              key={movie.title}
+              className="movieCard"
+              data-testid="movieCard"
+            >
+              <img src={movie.thumbnail} alt="" />
+            </article>
+          ))}
+        </section>
+      </Carousel>
     </section>
-  )
-}
+  );
+};
