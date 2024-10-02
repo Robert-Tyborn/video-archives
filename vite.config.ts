bc1289ffa9e9d781e22c221167e4467555e91554
Smustus <rikardfors_91@hotmail.com>
2024-10-02 11:24:06 +0200

    Update vite.config.ts
    grub not understand documents well, grub make change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/video-achives/',
   plugins: [react()],
   test: {
     globals: true,
