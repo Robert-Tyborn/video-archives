0c01ad4d5c76c2840298c0b1c8b38d66a258dc90
Smustus <rikardfors_91@hotmail.com>
2024-10-13 18:56:11 +0200

    adjusted movieCard test and small adjustment to styling
    added a different color scheme to the search bar to increase visiblity, adjusted bookmark page to only show 1 title and center text, and added a movieCard test to check if useNavigate is called. Lastly, removed commented out code from filmviewTest

--- src/components/movieCard/moviecard.test.tsx
+++ src/components/movieCard/moviecard.test.tsx
@@ -1,28 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { act, render, screen } from '@testing-library/react';
-import { fetchMovieData } from '../../utilities/fetch';
+import { render, screen } from '@testing-library/react';
 import mockMovies from '../../../public/data/movies.json';
 import MovieCard from './MovieCard';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, useNavigate } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
-vi.mock('../../utilities/fetch', () => ({
-  fetchMovieData: vi.fn(),
-}));
+vi.mock('react-router-dom', async () => {
+  const actual =
+    await vi.importActual<typeof import('react-router-dom')>(
+      'react-router-dom'
+    );
+  return {
+    ...actual,
+    useNavigate: vi.fn(),
+  };
+});
 
 describe('renders the moviecard component', () => {
   const mockMovie = mockMovies[0];
 
-  beforeEach(async () => {
-    (fetchMovieData as any).mockResolvedValue(mockMovies);
-    await act(async () => {
-      render(
-        <MemoryRouter>
-          <MovieCard movie={mockMovie} size="small" />
-        </MemoryRouter>
-      );
-    });
-  });
   it('should render the correct data in the moviecard', () => {
+    render(
+      <MemoryRouter>
+        <MovieCard movie={mockMovie} size="small" />
+      </MemoryRouter>
+    );
     const movieCard = screen.getByTestId('movieCard');
     expect(movieCard).toBeInTheDocument();
     expect(screen.getByAltText(mockMovie.title)).toBeInTheDocument();
@@ -31,5 +33,26 @@ describe('renders the moviecard component', () => {
     expect(movieImage).toHaveAttribute('src', mockMovie.thumbnail);
     expect(screen.getByText(`Released: ${mockMovie.year}`)).toBeInTheDocument();
     expect(screen.getByText(mockMovie.rating)).toBeInTheDocument();
+  });
+
+  it('should navigate to FilmView with correct movie data on card click', async () => {
+    const navigateMock = vi.mocked(useNavigate);
+    navigateMock.mockReturnValue(vi.fn());
+
+    vi.mocked(useNavigate).mockReturnValue(navigateMock);
+
+    render(
+      <MemoryRouter>
+        <MovieCard movie={mockMovie} size="small" />
+      </MemoryRouter>
+    );
+
+    const movieCard = screen.getByTestId('movieCard');
+    await userEvent.click(movieCard);
+
+    expect(navigateMock).toHaveBeenCalledWith(
+      `/video-archives/filmview/${mockMovie.title}`,
+      { state: mockMovie }
+    );
   });
 });
